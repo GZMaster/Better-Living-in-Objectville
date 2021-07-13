cf20f3e4e5488a0bec88b420c00ce539b465ad28
GZMaster <danigamester@gmail.com>
2021-07-13 12:55:38 +0100

    Initial commit

---
+++ src/com/company/MonsterTestDrive.java
@@ -0,0 +1,91 @@
+package com.company;
+
+public class MonsterTestDrive {
+
+    public static void main(String[] args){
+
+        Monster [] ma = new Monster[3];
+        ma[0] = new Vampire();
+        ma[1] = new Dragon();
+        ma[2] = new Monster();
+
+        for(int x = 0; x < 3; x++) {
+
+            ma[x].frighten(x);
+
+        }
+
+    }
+
+}
+
+class Monster{
+
+    //first code
+    boolean frighten(int d) {
+        System.out.println("arrrgh");
+        return true;
+    }
+
+    //second code
+//    boolean frighten(int x){
+//        System.out.println("arrrgh");
+//        return true;
+//    }
+
+    //third code
+//    boolean frighten(int x){
+//        System.out.println("arrrrgh");
+//        return false;
+//    }
+
+    //fourth code
+//    boolean frighten(int z){
+//        System.out.println("arrrgh");
+//        return true;
+//    }
+
+}
+
+class Vampire extends Monster{
+
+    //first code
+    boolean frighten(int x) {
+        System.out.println("a bite");
+        return false;
+    }
+
+    //second code
+//    int frighten(int f){
+//        System.out.println("a bite");
+//        return 1;
+//    }
+
+    //third code
+//    boolean scare(int x){
+//        System.out.println("a bite");
+//        return true;
+//    }
+
+    //fourth code
+//    boolean frighten(byte z){
+//        System.out.println("a bite");
+//        return true;
+//    }
+
+}
+
+class Dragon extends Monster{
+
+    boolean frighten(int degree) {
+
+        System.out.println("breath fire");
+        return true;
+
+    }
+
+}
+
+
+
+
